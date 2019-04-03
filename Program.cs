1b7184dd615e49b21f6eac57966db2b2b6126246
Hari Prasath Manokaran <38764929+hari5719@users.noreply.github.com>
2019-04-02 17:24:05 -0700

    Hungry Ninja code added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Human
 {
     class Human
@@ -39,17 +39,111 @@ namespace Human
         }
     }
 
+    class Food
+    {
+        public string Name;
+        public int Calories;
+        // Foods can be Spicy and/or Sweet
+        public bool IsSpicy; 
+        public bool IsSweet; 
+
+
+        public Food(string name, int cals, bool spice, bool sweet)
+        {
+            Name = name;
+            Calories = cals;
+            IsSpicy = spice;
+            IsSweet = sweet;
+        }
+
+    }
+
+    class Buffet
+    {
+        public List<Food> Menu;
+        
+        
+        //constructor
+        public Buffet()
+        {
+            Menu = new List<Food>()
+            {
+               new Food("Example1", 1000, false, false),
+               new Food("Example2", 1000, false, false),
+               new Food("Example3", 1000, false, false),
+               new Food("Example4", 1000, false, false),
+               new Food("Example5", 1000, false, false),
+               new Food("Example6", 1000, false, false),
+               new Food("Example7", 1000, false, false),
+            };
+        }
+     
+        public Food Serve()
+        {
+            Random rand = new Random();
+            int foodchoice = rand.Next(0,Menu.Count);
+        
+            return Menu[foodchoice];
+        }
+    }
+
+    class Ninja
+    {
+        private int calorieIntake;
+        public List<Food> FoodHistory;
+
+        public Ninja()
+        {
+            calorieIntake = 0;
+            FoodHistory = new List<Food>(); 
+        }
+     public bool IsFull
+    {
+        get
+        {
+            if(calorieIntake>1200)
+            {
+                return true;
+            }
+            else
+            {
+               return false;
+            }
+        }
+    }     
+        public void Eat(Food item)
+        {
+            if(IsFull==false)
+            {
+                calorieIntake += item.Calories;
+                FoodHistory.Add(item);
+                Console.WriteLine(calorieIntake);
+            }
+            else
+            {
+                Console.WriteLine("Ninja is FULL");
+            }
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-          var bill = new Human("Bill", 5,99,10,100);
-          var gates = new Human("Gates", 4,96,10,100);
+        //   var bill = new Human("Bill", 5,99,10,100);
+        //   var gates = new Human("Gates", 4,96,10,100);
 
-          gates.Attack(bill);
+        //   gates.Attack(bill);
 
-          Console.WriteLine(bill.Health);
-          
+        //   Console.WriteLine(bill.Health);
+          var buffet = new Buffet();
+          var ninja = new Ninja();
+          var food = buffet.Serve();
+          ninja.Eat(food);
+          ninja.Eat(food);
+          ninja.Eat(food);
+
         }
     }
 }
